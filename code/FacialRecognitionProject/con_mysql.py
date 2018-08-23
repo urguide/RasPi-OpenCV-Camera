0a1dc79316fb32332101369f0c81e5df0c670659
urguide <urguide@gmail.com>
2018-08-23 20:33:17 +0800

    Second Commit

---
+++ code/FacialRecognitionProject/con_mysql.py
@@ -0,0 +1,90 @@
+''''
+Real Time Face Recogition
+	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> LBPH computed model (trained faces) should be on trainer/ dir
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
+
+'''
+
+import cv2
+import numpy as np
+import os 
+from sqlalchemy import create_engine
+import time
+from sqlalchemy.orm import sessionmaker
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W'] 
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+
+
+while True:
+
+    ret, img =cam.read()
+    img = cv2.flip(img, -1) # Flip vertically
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+
+        # Check if confidence is less them 100 ==> "0" is perfect match 
+        if (confidence < 100):
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+    engine = create_engine("mysql+pymysql://iii:238914@vz.pride.tw:3306")
+    engine.execute("USE pi;")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    comer = pi(timestamp=int(time.time()),datetime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), id = '', name='tom',in_store=True,item1 = '',item2='',item3='',item4='',item5='')
+    session.add(comer)
+    session.commit()
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
